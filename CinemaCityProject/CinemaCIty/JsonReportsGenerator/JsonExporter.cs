affae5b3f392900f853417acea5dab0c2b4fb1b1
YavorYanchev <yavor_yanchev_bs@abv.bg>
2016-11-07 11:58:02 +0200

    Added Json Exporter

---
+++ CinemaCityProject/CinemaCIty/JsonReportsGenerator/JsonExporter.cs
@@ -0,0 +1,42 @@
+﻿using CinemaCity.Model.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonReportsGenerator
+{
+    public static class JsonExporter
+    {
+        public static void Export()
+        {
+            var db = new CinemaCityEntities();
+
+            var projections = db.ProjectionMovies
+                         .Select(pm => new
+                         {
+                             id = pm.Id,
+                             name = pm.Movy.Title,
+                             start = pm.Projection.Start,
+                             cinema = pm.Projection.ProjectionRoom.Cinema.Name,
+                             price = pm.Projection.Price,
+                             rating = pm.Movy.Rating,
+                             genre = pm.Movy.Type,
+                             minutes = pm.Movy.Minutes
+                         });
+
+            foreach (var proj in projections)
+            {
+                var jsonObject = JsonConvert.SerializeObject(proj, Newtonsoft.Json.Formatting.Indented);
+                var address = "../../../ExportedJson/" + proj.id + ".json";
+                File.WriteAllText(address, jsonObject);
+            }
+
+        }
+
+
+    }
+}
--- CinemaCityProject/CinemaCIty/XMLLoaderConsoleTester/Program.cs
+++ CinemaCityProject/CinemaCIty/XMLLoaderConsoleTester/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace XMLLoaderConsoleTester
+﻿using JsonReportsGenerator;
+
+namespace XMLLoaderConsoleTester
 {
     class Program
     {
         static void Main(string[] args)
         {
             var test = new XMLDataLoader.XmlDataLoader("");
-            test.WriteSQLDataToXML();
+            //test.WriteSQLDataToXML();
+
+            //json export
+           JsonExporter.Export();
         }
     }
 }
